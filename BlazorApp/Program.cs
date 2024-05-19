84bcd31a072e65922a778a80cede7df709d19fe6
Lars Gerrit Kliesing (LGK) <lgk@live.de>
2024-05-19 12:18:16 +0200

    Some theming

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,13 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddFluentUIComponents();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.RootComponents.RegisterCustomElement<MyTodo>("my-todo");
+builder.RootComponents.RegisterCustomElement<MyTheme>("my-theme");
 
 await builder.Build().RunAsync();
--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <my-todo Title="From React app as Web Component"></my-todo>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <my-theme></my-theme>
+      <my-todo
+        Title="From React app as Web Component"
+        AccentTone="#ff0000"
+      ></my-todo>
     </div>
   );
 }
