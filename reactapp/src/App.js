18031b606d976680909c303952379b333fbc453f
Lars Gerrit Kliesing (LGK) <lgk@live.de>
2024-05-19 12:52:28 +0200

    Deploy

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,17 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <my-theme></my-theme>
-      <my-todo
-        Title="From React app as Web Component"
-        AccentTone="#ff0000"
-      ></my-todo>
-    </div>
-  );
+    return (
+        <div className="App">
+            <img src={logo} className="App-logo" alt="logo" />
+            <my-theme></my-theme>
+            <my-todo
+                Title="From React app as Web Component"
+                AccentTone="#ff0000"
+            ></my-todo>
+        </div>
+    )
 }
 
-export default App;
+export default App
